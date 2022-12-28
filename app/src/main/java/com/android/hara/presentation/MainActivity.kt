0e992286806713089c87ef73696cab97c602197f
IslandofDream <rhffldkt0517@naver.com>
2022-12-29 00:52:24 +0900

    [Feat] add presentation package

--- app/src/main/java/com/android/hara/presentation/MainActivity.kt
+++ app/src/main/java/com/android/hara/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.hara
+package com.android.hara.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.hara.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
