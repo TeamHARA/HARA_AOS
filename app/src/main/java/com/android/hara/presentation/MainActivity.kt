da1ece793d68f754555f5a8366868d31ec57ff21
IslandofDream <rhffldkt0517@naver.com>
2022-12-29 01:11:07 +0900

    [Feat] add gradle for using local.properties

--- app/src/main/java/com/android/hara/presentation/MainActivity.kt
+++ app/src/main/java/com/android/hara/presentation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.android.hara.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.android.hara.BuildConfig
 import com.android.hara.R
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.e(BuildConfig.BASE_URL)
     }
 }
