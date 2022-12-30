c320ffdf02c6daeef99247d58c051cda6e620c2c
junwoo <rhffldkt0517@naver.com>
2022-12-30 21:29:53 +0900

    [Feat] Add 고민 상세보기, 고민 최종결정 패키지 및 액티비티

---
+++ app/src/main/java/com/android/hara/presentation/detail/DecisionActivity.kt
@@ -0,0 +1,14 @@
+package com.android.hara.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.hara.R
+import com.android.hara.databinding.ActivityDecisionBinding
+import com.android.hara.presentation.base.BindingActivity
+
+class DecisionActivity : BindingActivity<ActivityDecisionBinding>(R.layout.activity_decision) {
+    //TODO [고민최종결정] 부부입니다. 추후 네이밍 변경 예정
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
