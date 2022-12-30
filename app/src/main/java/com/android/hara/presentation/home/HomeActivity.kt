99448098e1f055e5e4f8c1b3475875bfd7115456
junwoo <rhffldkt0517@naver.com>
2022-12-30 21:05:48 +0900

    [Feat] Apply BindingActivity to Home Activity

--- app/src/main/java/com/android/hara/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/android/hara/presentation/home/HomeActivity.kt
@@ -3,10 +3,11 @@ package com.android.hara.presentation.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.hara.R
+import com.android.hara.databinding.ActivityHomeBinding
+import com.android.hara.presentation.base.BindingActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
     }
 }
