ff6b628f91940efb7499a57ddbaa816d677eefc4
junwoo <rhffldkt0517@naver.com>
2022-12-30 21:05:16 +0900

    [Feat] Add Fragment with home activity

---
+++ app/src/main/java/com/android/hara/presentation/home/fragment/TogetherFragment.kt
@@ -0,0 +1,14 @@
+package com.android.hara.presentation.home.fragment
+
+import android.os.Bundle
+import android.view.View
+import com.android.hara.R
+import com.android.hara.databinding.FragmentTogetherBinding
+import com.android.hara.presentation.base.BindingFragment
+
+class TogetherFragment :BindingFragment<FragmentTogetherBinding>(R.layout.fragment_together) {
+    //TODO [함께 해라] 프래그먼트 네이밍 구리니까 추천받음
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
