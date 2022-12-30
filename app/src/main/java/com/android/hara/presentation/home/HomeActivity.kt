de4809a2c1ddbaf5851bfe96a5a2db3b704c680a
junwoo <rhffldkt0517@naver.com>
2022-12-31 00:02:11 +0900

    [Feat] Apply Bottom Navigation Graph

--- app/src/main/java/com/android/hara/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/android/hara/presentation/home/HomeActivity.kt
@@ -1,7 +1,8 @@
 package com.android.hara.presentation.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.android.hara.R
 import com.android.hara.databinding.ActivityHomeBinding
 import com.android.hara.presentation.base.BindingActivity
@@ -10,5 +11,15 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home
     //TODO 바텀 네비게이션 및 [함께해라], [보관함] 프래그먼트가 들어갈 HomeAcitity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navGraph = navController.navInflater.inflate(R.navigation.bottom_nav_graph)
+        navController.graph = navGraph
+        binding.bottomNavHome   .setupWithNavController(navController)
     }
 }
