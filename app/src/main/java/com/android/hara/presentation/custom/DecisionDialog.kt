70ef87c5a7e61ed4cbc7ace6d3eae472999e2053
junwoo <rhffldkt0517@naver.com>
2023-01-04 20:26:14 +0900

    [Feat] Write Dialog Class

---
+++ app/src/main/java/com/android/hara/presentation/custom/DecisionDialog.kt
@@ -0,0 +1,53 @@
+package com.android.hara.presentation.custom
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import com.android.hara.databinding.DialogDecisionBinding
+import com.android.hara.presentation.custom.model.DialogData
+import com.android.hara.presentation.util.dpToPx
+
+class DecisionDialog(
+    val context: Context,
+    val data: DialogData,
+    private val itemActionListener: (String) -> Unit
+) {
+
+    private val inflater by lazy { LayoutInflater.from(context) }
+    private val binding: DialogDecisionBinding = DialogDecisionBinding.inflate(inflater)
+    private lateinit var dialog: Dialog
+
+    init {
+        with(binding) {
+            this.title = data.title
+            this.warn = data.warn
+            this.action = data.action
+            this.cancel = data.cancel
+        }
+    }
+
+    fun showDialog() {
+        dialog = Dialog(context)
+        dialog.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setContentView(binding.root)
+            val params: WindowManager.LayoutParams = dialog.window!!.attributes
+            window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 배경색 투명
+
+            params.width = WindowManager.LayoutParams.MATCH_PARENT
+            params.height = WindowManager.LayoutParams.WRAP_CONTENT
+            (binding.root.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                marginStart = 12.dpToPx(binding.root.context)
+                marginEnd = 12.dpToPx(binding.root.context)
+            } // 레이아웃 속성 재 적용
+            dialog.window!!.attributes = params
+        }
+        //TODO setListener(itemAddListener, item)
+        dialog.show()
+    }
+}
--- app/src/main/java/com/android/hara/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/android/hara/presentation/detail/DetailActivity.kt
@@ -4,10 +4,22 @@ import android.os.Bundle
 import com.android.hara.R
 import com.android.hara.databinding.ActivityDetailBinding
 import com.android.hara.presentation.base.BindingActivity
+import com.android.hara.presentation.custom.DecisionDialog
+import com.android.hara.presentation.custom.model.DialogData
 
-class DetailActivity:BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
     //TODO [고민글 상세보기] 부분입니다. 추후 네미망 변경 예정
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.tvDetailCategory.setOnClickListener {
+            DecisionDialog(
+                this, DialogData(
+                    getString(R.string.anonymity),
+                    getString(R.string.anonymity),
+                    getString(R.string.anonymity),
+                    getString(R.string.anonymity)
+                )
+            ){}.showDialog()
+        }
     }
 }
