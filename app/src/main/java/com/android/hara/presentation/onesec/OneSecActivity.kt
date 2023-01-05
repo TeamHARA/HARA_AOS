d0865a0cea684edcd8292647b1d059f34eefdb85
junwoo <rhffldkt0517@naver.com>
2023-01-05 18:17:41 +0900

    [Ui] Rename class name / Compose eidttext inputlayout

--- app/src/main/java/com/android/hara/presentation/onesec/OneSecActivity.kt
+++ app/src/main/java/com/android/hara/presentation/onesec/OneSecActivity.kt
@@ -1,12 +1,11 @@
-package com.android.hara.presentation.cardgame
+package com.android.hara.presentation.onesec
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.hara.R
-import com.android.hara.databinding.ActivityCardGameBinding
+import com.android.hara.databinding.ActivityOneSecBinding
 import com.android.hara.presentation.base.BindingActivity
 
-class CardGameActivity : BindingActivity<ActivityCardGameBinding>(R.layout.activity_card_game) {
+class OneSecActivity : BindingActivity<ActivityOneSecBinding>(R.layout.activity_one_sec) {
     //TODO [보관함]에서 넘어와 [1초만에 고민해결하기] 부분 액티비티 입니다. 추후 네이밍 변경예정
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
