d6fdbd49fab1a736925e9e3a95ea76dba0cb7dae
junwoo <rhffldkt0517@naver.com>
2023-01-06 02:19:46 +0900

    [Feat] Add Databinding

--- app/src/main/java/com/android/hara/presentation/onesec/viewmodel/OneSecViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/onesec/viewmodel/OneSecViewModel.kt
@@ -4,10 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OneSecViewModel : ViewModel() {
-    private val _enabled = MutableLiveData<Boolean>(false)
-    val enabled get() = _enabled
+    private val _solution = MutableLiveData<String>()
+    val solution get() = _solution
 
     private val _worry = MutableLiveData<String>()
     val worry get() = _worry
-
 }
