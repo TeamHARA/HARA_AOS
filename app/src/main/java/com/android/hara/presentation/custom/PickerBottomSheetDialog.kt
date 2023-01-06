784ace167db2a9a8b156efeec10b886fe638d1c1
junwoo <rhffldkt0517@naver.com>
2023-01-06 19:18:48 +0900

    [Ui] Add BindingBottomSheetDialog

---
+++ app/src/main/java/com/android/hara/presentation/custom/PickerBottomSheetDialog.kt
@@ -0,0 +1,9 @@
+package com.android.hara.presentation.custom
+
+import com.android.hara.R
+import com.android.hara.databinding.FragmentBottomsheetPickerBinding
+import com.android.hara.presentation.base.BindingBottomSheetDialog
+
+class PickerBottomSheetDialog :
+    BindingBottomSheetDialog<FragmentBottomsheetPickerBinding>(R.layout.fragment_bottomsheet_picker) {
+}
