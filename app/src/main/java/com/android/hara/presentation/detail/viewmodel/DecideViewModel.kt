b6ff40c23d91e466488dfe848e38bee3477eee6a
Skylar <twinkle991@naver.com>
2023-01-06 18:01:09 +0900

    [UI] 최종고민/고민해결 뷰 완료

--- app/src/main/java/com/android/hara/presentation/detail/viewmodel/DecideViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/detail/viewmodel/DecideViewModel.kt
@@ -1,6 +1,9 @@
 package com.android.hara.presentation.detail.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DecideViewModel: ViewModel() {
+    private val _enabled = MutableLiveData<Boolean>(false)
+    val enabled get() = _enabled
 }
--- app/src/main/java/com/android/hara/presentation/home/fragment/storage/StorageFragment.kt
+++ app/src/main/java/com/android/hara/presentation/home/fragment/storage/StorageFragment.kt
@@ -1,10 +1,8 @@
 package com.android.hara.presentation.home.fragment.storage
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.android.hara.R
 import com.android.hara.databinding.FragmentStorageBinding
 import com.android.hara.presentation.base.BindingFragment
@@ -13,5 +11,9 @@ import com.android.hara.presentation.base.BindingFragment
 class StorageFragment : BindingFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStorageOnesec.setOnClickListener {
+
+        }
     }
 }
