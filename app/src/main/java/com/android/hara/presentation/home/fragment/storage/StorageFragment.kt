85a1dfc77ee2ca973b462f419ef72e1f08dc8353
Skylar <twinkle991@naver.com>
2023-01-06 18:54:58 +0900

    [UI] 프래그먼트 뷰 id 수정

--- app/src/main/java/com/android/hara/presentation/home/fragment/storage/StorageFragment.kt
+++ app/src/main/java/com/android/hara/presentation/home/fragment/storage/StorageFragment.kt
@@ -6,14 +6,18 @@ import android.view.View
 import com.android.hara.R
 import com.android.hara.databinding.FragmentStorageBinding
 import com.android.hara.presentation.base.BindingFragment
+import com.android.hara.presentation.onesec.OneSecActivity
 
 
 class StorageFragment : BindingFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnStorageOnesec.setOnClickListener {
-//TODO 1초만에 해결하기 로 연결해주기
+        // 버튼 클릭 시 '1초만에 해결하기' 액티비티로 연결
+        binding.cvBtnOnesec.setOnClickListener {
+            val intent = Intent(activity, OneSecActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION) // 이거 지워도 되나..
+            startActivity(intent)
         }
     }
 }
