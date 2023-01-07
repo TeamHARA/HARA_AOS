009518f1d439babddb843f9bd2b42676b5f7aeee
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 01:51:24 +0900

    [Feat] Add Hilt HiltViewModel Annotation

--- app/src/main/java/com/android/hara/presentation/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/home/viewmodel/HomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.android.hara.presentation.home.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class HomeViewModel :ViewModel(){
 }
--- app/src/main/java/com/android/hara/presentation/onesec/viewmodel/OneSecViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/onesec/viewmodel/OneSecViewModel.kt
@@ -2,7 +2,9 @@ package com.android.hara.presentation.onesec.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class OneSecViewModel : ViewModel() {
     private val _solution = MutableLiveData<String>()
     val solution get() = _solution
