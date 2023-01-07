be0f5036912d8ae39c2957a1b08a12ceb27d812f
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 03:20:38 +0900

    [Feat] Add Reqres Server Connection in viewModel

--- app/src/main/java/com/android/hara/data/model/response/ResponseReqresListDTO.kt
+++ app/src/main/java/com/android/hara/data/model/response/ResponseReqresListDTO.kt
@@ -1,41 +1,44 @@
 package com.android.hara.data.model.response
 
+
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-class ResponseReqresListDTO(
+@Serializable
+data class ResponseReqresListDTO(
+    //TODO 삭제예정
     @SerialName("page")
-    val page: Int?, // 2
+    val page: Int, // 2
     @SerialName("per_page")
-    val perPage: Int?, // 6
+    val perPage: Int, // 6
     @SerialName("total")
-    val total: Int?, // 12
+    val total: Int, // 12
     @SerialName("total_pages")
-    val totalPages: Int?, // 2
+    val totalPages: Int, // 2
     @SerialName("data")
-    val `data`: List<Data?>?,
+    val `data`: List<Data>,
     @SerialName("support")
-    val support: Support?
+    val support: Support
 ) {
     @Serializable
     data class Data(
         @SerialName("id")
-        val id: Int?, // 7
+        val id: Int, // 7
         @SerialName("email")
-        val email: String?, // michael.lawson@reqres.in
+        val email: String, // michael.lawson@reqres.in
         @SerialName("first_name")
-        val firstName: String?, // Michael
+        val firstName: String, // Michael
         @SerialName("last_name")
-        val lastName: String?, // Lawson
+        val lastName: String, // Lawson
         @SerialName("avatar")
-        val avatar: String? // https://reqres.in/img/faces/7-image.jpg
+        val avatar: String // https://reqres.in/img/faces/7-image.jpg
     )
 
     @Serializable
     data class Support(
         @SerialName("url")
-        val url: String?, // https://reqres.in/#support-heading
+        val url: String, // https://reqres.in/#support-heading
         @SerialName("text")
-        val text: String? // To keep ReqRes free, contributions towards server costs are appreciated!
+        val text: String // To keep ReqRes free, contributions towards server costs are appreciated!
     )
 }
--- app/src/main/java/com/android/hara/presentation/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/home/viewmodel/HomeViewModel.kt
@@ -1,11 +1,45 @@
 package com.android.hara.presentation.home.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.hara.data.model.request.RequestReqresUserDTO
 import com.android.hara.domain.repository.HARARepository
+import com.android.hara.domain.repository.ReqresRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val HARARepository:HARARepository)
+class HomeViewModel @Inject constructor(private val ReqresRepository:ReqresRepository)
     :ViewModel(){
+
+    //TODO 교체예정
+    init {
+            viewModelScope.launch {
+                runCatching {
+                    ReqresRepository.getReqres()
+                 }.onSuccess {
+                    if(it.isSuccessful) // 내부 코드보면 응답코드 200~299를 의미
+                        Timber.e("Success")
+                    else // 응답코드 400~599
+                        Timber.e("Failure")
+                }.onFailure {
+                    Timber.e(it)
+                }
+
+                runCatching {
+                    //ReqresRepository.postReqres(RequestReqresUserDTO("morpheus","leader"))
+                    ReqresRepository.postReqres(RequestReqresUserDTO("morpheus","leader"))
+                }.onSuccess {
+                    if(it.isSuccessful) // 내부 코드보면 응답코드 200~299를 의미
+                        Timber.e("Success")
+                    else // 응답코드 400~599
+                        Timber.e("Failure")
+                }.onFailure { //데이터 없음 등등으로 인한 서버통신 자체가 실패한 경우
+                    Timber.e(it)
+                }
+
+            }
+        }
 }
