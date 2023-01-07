36b848142b3bb1688aebec72f5719a8814f1c5f7
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 02:22:23 +0900

    [Feat] Add HaraService for vote - 예시를 위한 게시글 투표 서비스 인터페이스 추가

--- app/src/main/java/com/android/hara/data/datasource/HaraService.kt
+++ app/src/main/java/com/android/hara/data/datasource/HaraService.kt
@@ -1,4 +1,14 @@
 package com.android.hara.data.datasource
 
+import com.android.hara.data.model.request.RequestVoteDTO
+import com.android.hara.data.model.response.ResponseVoteDTO
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface HaraService {
+    // 어떠한 액션을 취할건지 가장 먼저 작성해야합니다.
+    @POST("/worry")
+    suspend fun vote(@Body request:RequestVoteDTO) : ResponseVoteDTO
+    // suspend fun vote(@Body request:RequestVoteDTO) : Response<ResponseVoteDTO>
+    // Response에서 별다른 걸 안해줘도 된다면 == 서버에서 status가 정확하게 온다며 Response 필요없음
 }
