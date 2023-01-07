51aa5deaeda09999bbe5a569440a0bc37d59320c
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 02:31:51 +0900

    [Feat] Add RepsoitoryModule

--- app/src/main/java/com/android/hara/di/RepositroyModule.kt
+++ app/src/main/java/com/android/hara/di/RepositroyModule.kt
@@ -1,10 +1,18 @@
 package com.android.hara.di
 
+import com.android.hara.data.repository.HARARepositoryImpl
+import com.android.hara.domain.repository.HARARepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindToHARARepository(HARARepositoryImpl: HARARepositoryImpl): HARARepository
+
 }
