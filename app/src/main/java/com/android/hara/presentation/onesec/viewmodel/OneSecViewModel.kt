c183691cd16d111bb288c4c001a4e0faf48af44e
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 02:59:29 +0900

    [Fix] Add Inject for viewmodels

--- app/src/main/java/com/android/hara/presentation/onesec/viewmodel/OneSecViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/onesec/viewmodel/OneSecViewModel.kt
@@ -3,9 +3,10 @@ package com.android.hara.presentation.onesec.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class OneSecViewModel : ViewModel() {
+class OneSecViewModel @Inject constructor() : ViewModel() {
     private val _solution = MutableLiveData<String>()
     val solution get() = _solution
 
