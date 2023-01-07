d7eb3c8ecdcaf14307d511769544633de1cfe0d9
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 02:33:18 +0900

    [Feat] Inject HARA Repository to HomeViemodel

--- app/src/main/java/com/android/hara/presentation/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/home/viewmodel/HomeViewModel.kt
@@ -1,8 +1,11 @@
 package com.android.hara.presentation.home.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.android.hara.domain.repository.HARARepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel :ViewModel(){
+class HomeViewModel @Inject constructor(private val HARARepository:HARARepository)
+    :ViewModel(){
 }
