07d6a4fbe3fa8ced5d0e017ddbe0976ff47f833d
IslandofDream <rhffldkt0517@naver.com>
2023-01-08 01:46:33 +0900

    [Feat] Add Constant

---
+++ app/src/main/java/com/android/hara/data/util/Constant.kt
@@ -0,0 +1,11 @@
+package com.android.hara.data.util
+
+object Constant {
+    const val APPLICATION_JSON = "application/json"
+
+    //sharedPreference
+    const val PREFERENCES_NAME = "SPOT_PREFERENCE"
+    const val PREF_ID = "ID"
+    const val PREF_PW = "PW"
+    const val PREF_NAME = "NAME"
+}
