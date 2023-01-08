754059e6adb2a2cdc68ecf852a4a7c8f5e2fea2c
junwoo <rhffldkt0517@naver.com>
2023-01-08 18:30:36 +0900

    [Feat] Add NumberPicekr Logic

--- app/src/main/java/com/android/hara/presentation/custom/PickerBottomSheetDialog.kt
+++ app/src/main/java/com/android/hara/presentation/custom/PickerBottomSheetDialog.kt
@@ -1,31 +1,39 @@
 package com.android.hara.presentation.custom
 
-import android.graphics.Typeface
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.TypefaceSpan
 import android.view.View
-import androidx.core.content.res.ResourcesCompat
 import com.android.hara.R
 import com.android.hara.databinding.FragmentBottomsheetPickerBinding
 import com.android.hara.presentation.base.BindingNotDraggableBottomSheet
+import com.android.hara.presentation.util.HARAobjcet.categoryList
+import timber.log.Timber
 
 
 class PickerBottomSheetDialog :
     BindingNotDraggableBottomSheet<FragmentBottomsheetPickerBinding>(R.layout.fragment_bottomsheet_picker) {
 
-    private val categoryList = listOf<String>("일상", "연애", "패션/뷰티", "커리어", "대학생", "진로고민", "메뉴고민")
+    private var count = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.pickerCategory.displayedValues = categoryList.toTypedArray()
         binding.pickerCategory.minValue = 0
-        binding.pickerCategory.maxValue = categoryList.size-1
+        binding.pickerCategory.maxValue = categoryList.size - 1
         binding.pickerCategory.displayedValues = categoryList.toTypedArray()
+
+
+        binding.pickerCategory.setOnValueChangedListener { picker, xoldVal, newVal ->
+            Timber.e(picker.toString())
+            Timber.e(xoldVal.toString())
+            Timber.e(newVal.toString())
+            count = newVal
+        }
 
         binding.btnCancel.setOnClickListener {
             dismiss()
+        }
+        binding.btnComplete.setOnClickListener {
+            Timber.e(count.toString())
         }
     }
 
