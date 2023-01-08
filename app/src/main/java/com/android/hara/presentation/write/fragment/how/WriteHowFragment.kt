861cfb9dbc283d2a0b5981af7c2ebf84ec17db1b
wkdyujin <yujin1203713@gmail.com>
2023-01-08 19:07:27 +0900

    [Feat] 작성_공개여부 로직 구현 완료

--- app/src/main/java/com/android/hara/presentation/write/fragment/how/WriteHowFragment.kt
+++ app/src/main/java/com/android/hara/presentation/write/fragment/how/WriteHowFragment.kt
@@ -3,6 +3,7 @@ package com.android.hara.presentation.write.fragment.how
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.android.hara.R
@@ -14,11 +15,40 @@ class WriteHowFragment :
     BindingFragment<FragmentWriteHowBinding>(R.layout.fragment_write_how) {
     lateinit var navController: NavController
     private val writeViewModel: WriteViewModel by activityViewModels()
+    private val howFragViewModel: HowFragViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setNavigation(view)
         onClickBackBtn()
+        setViewModel()
+
+        setWorryBtn()
+        setWithBtn()
+    }
+
+    private fun setWorryBtn() {
+        binding.clCategoryWorryAloneButton.setOnClickListener {
+            howFragViewModel.isAloneSelected.value = !howFragViewModel.isAloneSelected.value!!
+            howFragViewModel.isWithSelected.value = false
+            setUploadBtn() // 버튼 누를 때마다 호출해줘야 함!
+        }
+    }
+
+    private fun setWithBtn() {
+        binding.clCategoryWorryWithButton.setOnClickListener {
+            howFragViewModel.isWithSelected.value = !howFragViewModel.isWithSelected.value!!
+            howFragViewModel.isAloneSelected.value = false
+            setUploadBtn()
+        }
+    }
+
+    private fun setUploadBtn() {
+        howFragViewModel.enabled.value = howFragViewModel.isWithSelected.value!! || howFragViewModel.isAloneSelected.value!!
+    }
+
+    private fun setViewModel() {
+        binding.vm = howFragViewModel
     }
 
     private fun setNavigation(view: View) {
