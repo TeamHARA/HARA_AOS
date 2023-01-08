054be098e2576ce70fc02021be527c779e815a36
Skylar <twinkle991@naver.com>
2023-01-08 20:44:14 +0900

    [FEAT] 옵션 선택 시 옵션, 버튼 스타일 변경 로직 추가 (데이터 바인딩 이용)

--- app/src/main/java/com/android/hara/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/android/hara/presentation/detail/DetailActivity.kt
@@ -1,14 +1,21 @@
 package com.android.hara.presentation.detail
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.android.hara.R
 import com.android.hara.databinding.ActivityDetailBinding
 import com.android.hara.presentation.base.BindingActivity
 import com.android.hara.presentation.custom.DecisionDialog
 import com.android.hara.presentation.custom.model.DialogData
+import com.android.hara.presentation.detail.viewmodel.DecideViewModel
+import com.android.hara.presentation.detail.viewmodel.DetailViewModel
+import timber.log.Timber
 
 class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
     //TODO [고민글 상세보기] 부분입니다. 추후 네이밍 변경 예정
+
+    private val detailVm by viewModels<DetailViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.appbarDetail.setNavigationOnClickListener {
@@ -24,5 +31,31 @@ class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_
                 )
             ) {}.showDialog()
         }
+
+        // n번째 옵션 클릭 시 옵션과 버튼 스타일 변하는 로직
+        binding.layoutOption1.clOptBox.setOnClickListener {
+            changeVmSnum(1)
+        }
+        binding.layoutOption2.clOptBox.setOnClickListener {
+            changeVmSnum(2)
+        }
+        binding.layoutOption3.clOptBox.setOnClickListener {
+            changeVmSnum(3)
+        }
+        binding.layoutOption4.clOptBox.setOnClickListener {
+            changeVmSnum(4)
+        }
+
+        detailVm.sNum.observe(this){
+            binding.detailVm = detailVm
+        }
+    }
+
+    // n번째 옵션이 선택되면 DetailViewModel 안의 sNum의 value가 n으로 바뀐다
+    private fun changeVmSnum(n: Int) { // n이 선택된 상태
+        // 1) n이 클릭되면: n만 비활성화돼야 해
+        if (detailVm.sNum.value == n) detailVm.sNum.value = 0
+        // 2) n이 클릭되면: n만 활성화돼야 해
+        else detailVm.sNum.value = n
     }
 }
