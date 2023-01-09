6c2337fbbe95acb4fdd38648f5223dc557ee83b0
wkdyujin <yujin1203713@gmail.com>
2023-01-10 04:01:17 +0900

    [Feat] 버튼 로직 구현

---
+++ app/src/main/java/com/android/hara/presentation/home/fragment/together/SimpleModel.kt
@@ -0,0 +1,6 @@
+package com.android.hara.presentation.home.fragment.together
+
+data class SimpleModel(
+    val title: String,
+    var isSelected: Boolean
+)
--- app/src/main/java/com/android/hara/presentation/home/fragment/together/TogetherFragment.kt
+++ app/src/main/java/com/android/hara/presentation/home/fragment/together/TogetherFragment.kt
@@ -1,26 +1,37 @@
 package com.android.hara.presentation.home.fragment.together
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.android.hara.R
 import com.android.hara.databinding.FragmentTogetherBinding
 import com.android.hara.presentation.base.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class TogetherFragment : BindingFragment<FragmentTogetherBinding>(R.layout.fragment_together) {
 
     private val category: Array<String>
         get() = resources.getStringArray(R.array.category_array)
+    private var list = arrayListOf<SimpleModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val categoryAdapter = CategoryAdapter(requireContext()) {
-            Log.d("TEST", it)
+        for (i in 0..7) {
+            list.add(SimpleModel(title = category[i], isSelected = false))
+        }
+
+        val categoryAdapter = CategoryAdapter(requireContext(), list).apply {
+            setOnItemClickListener(object : CategoryAdapter.OnItemClickListener {
+                override fun onItemClick(item: SimpleModel, position: Int) {
+                    Timber.e(item.title)
+                }
+            })
         }
         binding.rvTogetherCategory.adapter = categoryAdapter
-        categoryAdapter.submitList(category.toList()) // 데이터를 넣어준다 (업데이트할 때에도)
+//        categoryAdapter.submitList(category.toList()) // 데이터를 넣어준다 (업데이트할 때에도)
+        binding.rvTogetherCategory.setHasFixedSize(true)
+        binding.rvTogetherCategory.itemAnimator = null
     }
 }
