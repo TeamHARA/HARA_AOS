e41b0857f192910e49df928cece48a4cab8ad9d5
junwoo <rhffldkt0517@naver.com>
2023-01-10 03:35:04 +0900

    [Feat] add item delete logic

--- app/src/main/java/com/android/hara/presentation/write/fragment/option/adapter/WriteOptionAdapter.kt
+++ app/src/main/java/com/android/hara/presentation/write/fragment/option/adapter/WriteOptionAdapter.kt
@@ -1,18 +1,31 @@
 package com.android.hara.presentation.write.fragment.option.adapter
 
+import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.android.hara.databinding.ItemAddOptionBinding
 import com.android.hara.databinding.ItemWriteOptionBinding
-import com.android.hara.presentation.util.GlobalDiffCallBack
+import com.android.hara.presentation.util.setOnSingleClickListener
 import com.android.hara.presentation.write.fragment.option.model.OptionData
+import timber.log.Timber
 
-class WriteOptionAdapter() :
-    ListAdapter<OptionData, RecyclerView.ViewHolder>(GlobalDiffCallBack()) {
+
+class WriteOptionAdapter(private val itemClickListener: (OptionData) -> Unit) :
+    ListAdapter<OptionData, RecyclerView.ViewHolder>(IngredientDiffCallBack) {
 
     private lateinit var inflater: LayoutInflater
+    private val titleList = mutableListOf<String?>("", "", "", "")
+
+
+    init {
+        setHasStableIds(true)// 포지션 값 해쉬값으로 저장
+    }
 
     class ItemAddOptionViewHolder(val binding: ItemAddOptionBinding) :
         RecyclerView.ViewHolder(binding.root)
@@ -38,17 +51,52 @@ class WriteOptionAdapter() :
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        @SuppressLint("RecyclerView") position: Int
+    ) {
+        //positoin은 0 부터
         val currentItem = getItem(position)
+
+        Timber.e(position.toString())
         when (holder.itemViewType) {
             OPTION -> {
                 with(holder as ItemOptionViewHolder) {
-                    binding.apply {
+                    binding.inputText = titleList[position]
+                    binding.etWriteOptionInput.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(
+                            charSequence: CharSequence, i: Int, i1: Int, i2: Int
+                        ) {
+                        }
+
+                        override fun onTextChanged(
+                            charSequence: CharSequence, i: Int, i1: Int, i2: Int
+                        ) {
+                            Timber.e(charSequence.toString())
+                            titleList[position] = charSequence.toString()
+                        }
+
+                        override fun afterTextChanged(editable: Editable) {}
+                    })
+                    if (position >= 2) {
+                        binding.ibOptionDeleteButton.visibility = View.VISIBLE
+                    }
+
+                    binding.ibOptionDeleteButton.setOnSingleClickListener {
+                        removeItem(position)
                     }
                 }
             }
             ADD -> {
                 with(holder as ItemAddOptionViewHolder) {
+                    if (currentList.size == 5) {
+                        binding.root.visibility = View.GONE
+                    } else {
+                        binding.root.visibility = View.VISIBLE
+                    }
+                    binding.root.setOnSingleClickListener {
+                        itemClickListener(currentItem)
+                    }
                 }
             }
         }
@@ -59,8 +107,37 @@ class WriteOptionAdapter() :
         else ADD
     }
 
+    private fun removeItem(position: Int) {
+        if (position == 2) {
+            titleList[2] = titleList[3]
+            titleList[3] = ""
+        } else {
+            titleList[3] = ""
+        }
+        val newList = currentList.toMutableList()
+        newList.removeAt(position)
+        submitList(newList)
+    }
+
     companion object {
         const val ADD = 1
         const val OPTION = 2
+
+        private object IngredientDiffCallBack : DiffUtil.ItemCallback<OptionData>() {
+            override fun areItemsTheSame(
+                oldItem: OptionData,
+                newItem: OptionData
+            ): Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(
+                oldItem: OptionData,
+                newItem: OptionData
+            ): Boolean {
+                return oldItem.title == newItem.title
+            }
+        }
+
     }
 }
