444bba4c3c7e8afaa8db251f33ce51a7008a5f1c
wkdyujin <yujin1203713@gmail.com>
2023-01-10 17:12:15 +0900

    [Feat] add get alone list impl

--- app/src/main/java/com/android/hara/data/repository/HARARepositoryImpl.kt
+++ app/src/main/java/com/android/hara/data/repository/HARARepositoryImpl.kt
@@ -2,8 +2,10 @@ package com.android.hara.data.repository
 
 import com.android.hara.data.datasource.HARAService
 import com.android.hara.data.model.request.RequestVoteDTO
+import com.android.hara.data.model.response.AloneListResDto
 import com.android.hara.data.model.response.ResponseVoteDTO
 import com.android.hara.domain.repository.HARARepository
+import retrofit2.Response
 import javax.inject.Inject
 
 class HARARepositoryImpl @Inject constructor(
@@ -11,5 +13,9 @@ class HARARepositoryImpl @Inject constructor(
 ) : HARARepository {
     override suspend fun postVote(request: RequestVoteDTO): ResponseVoteDTO {
         return HARAService.vote(request)
+    }
+
+    override suspend fun getAloneList(isSolved: Int): Response<AloneListResDto> {
+        return HARAService.getAloneList(isSolved)
     }
 }
