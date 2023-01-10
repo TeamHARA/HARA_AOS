f855902b017e6bdaecf086bd2cf19b740d1c60c2
junwoo <rhffldkt0517@naver.com>
2023-01-10 17:46:29 +0900

    [Feat] Add ViewModel value

--- app/src/main/java/com/android/hara/presentation/write/fragment/category/CategoryFregViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/write/fragment/category/CategoryFregViewModel.kt
@@ -1,3 +1,12 @@
 package com.android.hara.presentation.write.fragment.category
 
-class CategoryFregViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryFregViewModel @Inject constructor() : ViewModel() {
+    private val _category = MutableLiveData<String>()
+    val category get() = _category
+}
