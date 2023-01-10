5b488c7f3e4004d349a63b1e045655b94d6b68df
wkdyujin <yujin1203713@gmail.com>
2023-01-10 19:28:20 +0900

    [Chore] 코드 정리

--- app/src/main/java/com/android/hara/presentation/home/fragment/storage/alone/StorageAloneFragment.kt
+++ app/src/main/java/com/android/hara/presentation/home/fragment/storage/alone/StorageAloneFragment.kt
@@ -8,25 +8,14 @@ import com.android.hara.R
 import com.android.hara.databinding.FragmentStorageSelfBinding
 import com.android.hara.presentation.base.BindingFragment
 import com.android.hara.presentation.home.fragment.storage.StorageAdapter
+import com.android.hara.presentation.home.fragment.storage.alone.StorageAloneViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
 class StorageAloneFragment :
     BindingFragment<FragmentStorageSelfBinding>(R.layout.fragment_storage_self) {
-
     private val storageAloneViewModel: StorageAloneViewModel by viewModels()
-
-//    private val dummyList = listOf<StorageData>(
-//        StorageData("일상", "우리집에 왜왔니 왜왔니 왜왔니", "2023.12.25", true),
-//        StorageData("사랑", "카레는 맛있어 나는 안 졸려", "2023.12.25", true),
-//        StorageData("일상", "우리집에 호이호이", "2023.12.25", true),
-//        StorageData("시하", "우리집에 갤럭시", "2023.12.25", true),
-//        StorageData("하시", "나나나나나나나나나나나마", "2023.12.25", false),
-//        StorageData("사과", "울라울라울라울라울라울라울라울라", "2023.12.25", false),
-//        StorageData("귤귤", "노트북노트북노트북노트북노트북노트북", "2023.12.25", false),
-//        StorageData("안드", "노틀담의꼽추노틀담의꼽추", "2023.12.25", false)
-//    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
