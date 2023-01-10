f0483300ab71ee5eed075a424098ab92e84a2613
wkdyujin <yujin1203713@gmail.com>
2023-01-10 19:16:25 +0900

    [Feat] 통신 응답값과 뷰 databinding 연결

--- app/src/main/java/com/android/hara/presentation/home/fragment/storage/alone/StorageAloneViewModel.kt
+++ app/src/main/java/com/android/hara/presentation/home/fragment/storage/alone/StorageAloneViewModel.kt
@@ -1,9 +1,10 @@
-package com.android.hara.presentation.home.fragment.storage
+package com.android.hara.presentation.home.fragment.storage.alone
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.android.hara.data.model.response.WorryListResDto
 import com.android.hara.domain.repository.HARARepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -11,10 +12,10 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class StorageViewModel @Inject constructor(private val haraRepository: HARARepository) :
+class StorageAloneViewModel @Inject constructor(private val haraRepository: HARARepository) :
     ViewModel() {
-    private val _getSuccess = MutableLiveData<Boolean>()
-    val getSuccess: LiveData<Boolean> = _getSuccess
+    private val _aloneData = MutableLiveData<List<WorryListResDto.Data>>()
+    val aloneData: LiveData<List<WorryListResDto.Data>> = _aloneData
 
     private val _isSolved = MutableLiveData<Int>(0)
     val isSolved: LiveData<Int> = _isSolved
@@ -26,6 +27,7 @@ class StorageViewModel @Inject constructor(private val haraRepository: HARARepos
             }.onSuccess {
                 if (it.isSuccessful) { // 내부 코드보면 응답코드 200~299를 의미
                     Timber.e("Success")
+                    _aloneData.value = it.body()!!.data
                 } else { // 응답코드 400~599
                     Timber.e("Failure")
                 }
--- app/src/main/java/com/android/hara/presentation/util/BindingConversion.kt
+++ app/src/main/java/com/android/hara/presentation/util/BindingConversion.kt
@@ -1,6 +1,5 @@
 package com.android.hara.presentation.util
 
-import android.graphics.drawable.Drawable
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.BindingAdapter
@@ -8,25 +7,28 @@ import com.android.hara.R
 
 @BindingAdapter("app:decide_selected")
 fun AppCompatButton.selected(sel: Boolean) {
-        if (sel) {
+    if (sel) {
         this.background = this.context.getDrawable(R.drawable.shape_rectangle_orange1_fill_30)
         this.setTextColor(this.context.getColor(R.color.white))
-    }
-    else {
+    } else {
         this.background = this.context.getDrawable(R.drawable.shape_rectangle_blue3_stroke_1_30)
         this.setTextColor(this.context.getColor(R.color.gray_2))
     }
 }
 
 @BindingAdapter("app:storage_flag")
-fun TextView.xmlIng(still: Boolean) {
+fun TextView.xmlIng(still: Boolean) { // still = xmlIng
     if (still) {
         this.background = this.context.getDrawable(R.drawable.shape_rectangle_orange2_stroke_1_4)
         this.setTextColor(this.context.getColor(R.color.orange_2))
         // 고민중이라는 문구
-    }
-    else {
+    } else {
         this.background = this.context.getDrawable(R.drawable.shape_rectanle_orange1_fill_4)
         this.setTextColor(this.context.getColor(R.color.white))
     }
+}
+
+@BindingAdapter("app:category_id")
+fun TextView.setCategory(categoryId: Int) {
+    this.text = resources.getStringArray(R.array.category_array)[categoryId]
 }
