798c1b5952104b733cf64423b7d1169f63760501
wkdyujin <yujin1203713@gmail.com>
2023-01-11 17:32:49 +0900

    [Feat] 코드 리뷰 반영 - 단일 뷰홀더 타입에서는 생성한 ViewHolder class로 ListAdapter 상속

--- app/src/main/java/com/android/hara/presentation/onesec/adapter/OneSecAdapter.kt
+++ app/src/main/java/com/android/hara/presentation/onesec/adapter/OneSecAdapter.kt
@@ -9,20 +9,20 @@ import com.android.hara.databinding.ItemLastWorryBinding
 import com.android.hara.presentation.util.GlobalDiffCallBack
 
 class OneSecAdapter :
-    ListAdapter<RandomListResDto.Data, RecyclerView.ViewHolder>(GlobalDiffCallBack()) {
+    ListAdapter<RandomListResDto.Data, OneSecAdapter.ItemViewHolder>(GlobalDiffCallBack()) {
 
     private lateinit var inflater: LayoutInflater
 
     class ItemViewHolder(val binding: ItemLastWorryBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         if (!::inflater.isInitialized) {
             inflater = LayoutInflater.from(parent.context)
         }
         return ItemViewHolder(ItemLastWorryBinding.inflate(inflater, parent, false))
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val curItem = getItem(position)
         with(holder as ItemViewHolder) {
             binding.worrydata = curItem
