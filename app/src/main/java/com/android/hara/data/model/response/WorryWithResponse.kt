6668ae1108de0b715308a41c3586d3fd5762c8e0
junwoo <rhffldkt0517@naver.com>
2023-01-11 23:38:46 +0900

    [Feat] Add PostResponseDto

---
+++ app/src/main/java/com/android/hara/data/model/response/WorryWithResponse.kt
@@ -0,0 +1,15 @@
+package com.android.hara.data.model.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WorryWithResponse(
+    @SerialName("message")
+    val message: String, // 고민글 작성 성공
+    @SerialName("status")
+    val status: Int, // 200
+    @SerialName("success")
+    val success: Boolean // true
+)
