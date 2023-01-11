33b6a718a524f45228b65e10b156db69461f17fb
junwoo <rhffldkt0517@naver.com>
2023-01-12 03:49:07 +0900

    [Refactor] Delete Reqres api

--- app/src/main/java/com/android/hara/di/RepositroyModule.kt
+++ app/src/main/java/com/android/hara/di/RepositroyModule.kt
@@ -1,9 +1,7 @@
 package com.android.hara.di
 
 import com.android.hara.data.repository.HARARepositoryImpl
-import com.android.hara.data.repository.ReqresRepositoryImpl
 import com.android.hara.domain.repository.HARARepository
-import com.android.hara.domain.repository.ReqresRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -16,10 +14,5 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindToHARARepository(HARARepositoryImpl: HARARepositoryImpl): HARARepository
-
-    //TODO 삭제예정
-    @Binds
-    @Singleton
-    abstract fun bindToReqresRepository(ReqresRepository: ReqresRepositoryImpl): ReqresRepository
 
 }
