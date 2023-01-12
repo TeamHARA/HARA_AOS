4bc5151a4dcf740ebaded79e17882e678fd7d5e8
Skylar <twinkle991@naver.com>
2023-01-12 14:06:21 +0900

    [FEAT] 이미지 가진 옵션 선택 시 이미지가 반응하는 로직 최대한 완성

--- app/src/main/java/com/android/hara/presentation/home/fragment/together/PostAdapter.kt
+++ app/src/main/java/com/android/hara/presentation/home/fragment/together/PostAdapter.kt
@@ -27,7 +27,8 @@ class PostAdapter(
     // view holder 객체를 생성한다
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         if (!::inflater.isInitialized) {
-            inflater = LayoutInflater.from(parent.context) // parent : adapter가 호출된 activity나 fragment
+            inflater =
+                LayoutInflater.from(parent.context) // parent : adapter가 호출된 activity나 fragment
         }
         return ItemPostViewHolder(ItemPostBinding.inflate(inflater, parent, false))
     }
@@ -54,27 +55,30 @@ class PostAdapter(
                 if (opt.hasImage) {
                     if (imgToOpt[0] == -1) {
                         imgToOpt[0] = opt.id
-                        binding.ivPostImg1.setImageResource(R.drawable.ic_bell)
+                        binding.ivPostImg1.setImageResource(R.drawable.img_card_result)
                         binding.ivPostImg1.visibility = View.VISIBLE
-                    }
-                    else if (imgToOpt[1] == -1) {
+                        binding.itImgSel1 = 0 // 초기값: 아무 옵션도 선택 안 된 상태, 둘다 제대로 보임
+                    } else if (imgToOpt[1] == -1) {
                         imgToOpt[1] = opt.id
-                        binding.ivPostImg2.setImageResource(R.drawable.ic_bell)
+                        binding.ivPostImg2.setImageResource(R.drawable.img_card_result)
                         binding.ivPostImg2.visibility = View.VISIBLE
+                        binding.itImgSel2 = 0 // 초기값: 아무 옵션도 선택 안 된 상태, 둘다 제대로 보임
                     }
-                    else ;
                 }
             }
-            if (imgToOpt[0] == -1 && imgToOpt[1] == -1) {
-                binding.ivPostImg1.visibility = View.GONE
-                binding.ivPostImg2.visibility = View.GONE
-            }
+            if (imgToOpt[0] == -1) binding.ivPostImg1.visibility = View.GONE
+            if (imgToOpt[1] == -1) binding.ivPostImg2.visibility = View.GONE
 
             // 1) 내가 쓴 글이면: '최종 결정하러 가기' 버튼
             if (curItem.isAuthor) {
                 binding.itOptClickable = false // 옵션 clickable = false
                 binding.itMyPost = true // 옵션에 check 표시 안 보임, '최종결정 하러가기' 버튼이 보임
                 binding.itOptSelNum = 0 // 이런 식으로 값을 안 주면 뷰가 재사용 될 때 itOptSelNum 값이 초기화가 안 돼서 옵션이 선택된 것처럼 나올 수도
+
+                // 내가 쓴 글은, 이미지가 무조건 선명하게 보이게
+                // 이미지를 갖는 옵션이 있는지, 어떤 옵션이 몇 번째 이미지 뷰를 갖는지 체크하는 로직은 없음
+                binding.itImgSel1 = 1
+                binding.itImgSel2 = 1
             }
 
             // 2) 내가 쓴 글이 아니면(남이 쓴 글이면): 투표 버튼
@@ -111,22 +115,23 @@ class PostAdapter(
                         optSelListener(curItem.worryId, curItem.option[0].id)
 
                         // [이미지 뷰] 옵션에 해당하는 이미지가 있었다면, 옵션 클릭 시 해당 이미지도 활성화
-                        clickImgActivate(binding, curItem.option[0], imgToOpt)
+                        clickImgActivate(binding, binding.itOptSelNum, curItem.option[0], imgToOpt)
+                        Timber.e(binding.itOptSelNum.toString())
                     }
                     binding.layoutPostOpt2.clPostOpt.setOnClickListener {
                         changeXmlOptSelNum(binding, 2)
                         optSelListener(curItem.worryId, curItem.option[1].id)
-                        clickImgActivate(binding, curItem.option[1], imgToOpt)
+                        clickImgActivate(binding, binding.itOptSelNum, curItem.option[1], imgToOpt)
                     }
                     binding.layoutPostOpt3.clPostOpt.setOnClickListener {
                         changeXmlOptSelNum(binding, 3)
                         optSelListener(curItem.worryId, curItem.option[2].id)
-                        clickImgActivate(binding, curItem.option[2], imgToOpt)
+                        clickImgActivate(binding, binding.itOptSelNum, curItem.option[2], imgToOpt)
                     }
                     binding.layoutPostOpt4.clPostOpt.setOnClickListener {
                         changeXmlOptSelNum(binding, 4)
                         optSelListener(curItem.worryId, curItem.option[3].id)
-                        clickImgActivate(binding, curItem.option[3], imgToOpt)
+                        clickImgActivate(binding, binding.itOptSelNum, curItem.option[3], imgToOpt)
                     }
 
                     binding.btnPostVote.setOnSingleClickListener {
@@ -152,38 +157,48 @@ class PostAdapter(
 
     // 각 item에, curItem의 option에 있는 title을 바인딩한다
     private fun setOptTitle(binding: ItemPostBinding, curItem: AllPostResDto.Data) {
-        // [옵션 1]
-        if (curItem.option.size >= 1)
+        if (curItem.option.size >= 1) { // [옵션 1]
             binding.layoutPostOpt1.tvPostOptTitle.text = curItem.option.get(0).title // text
-        // [옵션 2]
-        if (curItem.option.size >= 2)
+            binding.layoutPostOpt1.clPostOpt.visibility = View.VISIBLE
+        }
+        if (curItem.option.size >= 2) { // [옵션 2]
             binding.layoutPostOpt2.tvPostOptTitle.text = curItem.option.get(1).title // text
-
-        // [옵션 3]
-        if (curItem.option.size >= 3)
+            binding.layoutPostOpt2.clPostOpt.visibility = View.VISIBLE
+        }
+        if (curItem.option.size >= 3) { // [옵션 3]
             binding.layoutPostOpt3.tvPostOptTitle.text = curItem.option.get(2).title // text
-        else
+            binding.layoutPostOpt3.clPostOpt.visibility = View.VISIBLE
+        } else
             binding.layoutPostOpt3.clPostOpt.visibility = View.GONE
-
-        // [옵션 4]
-        if (curItem.option.size >= 4)
+        if (curItem.option.size >= 4) { // [옵션 4]
             binding.layoutPostOpt4.tvPostOptTitle.text = curItem.option.get(3).title // text
-        else
+            binding.layoutPostOpt4.clPostOpt.visibility = View.VISIBLE
+        } else
             binding.layoutPostOpt4.clPostOpt.visibility = View.GONE
     }
 
     private fun clickImgActivate(
         binding: ItemPostBinding,
+        itOptSelNum: Int,
         curOpt: AllPostResDto.Data.Option,
         imgToOpt: List<Int>
     ) {
-        if (curOpt.hasImage) {
-            if (curOpt.id == imgToOpt[0]) { // img1만 활성화
-                binding.ivPostImg2.alpha = 0.5F
+        if (curOpt.hasImage) { // 해당 옵션이 이미지를 가진다
+            if (itOptSelNum == 0) { // 아무 옵션도 선택 안 된 상태
+                binding.itImgSel1 = 0
+                binding.itImgSel2 = 0
+            } else { // 어떤 옵션이 선택된 상태
+                if (curOpt.id == imgToOpt[0]) { // 그 옵션이 img1에 해당되면: img1만 활성화
+                    binding.itImgSel1 = 1
+                    binding.itImgSel2 = 0
+                } else if (curOpt.id == imgToOpt[1]) { // 그 옵션이 img2에 해당되면: img2만 활성화
+                    binding.itImgSel1 = 0
+                    binding.itImgSel2 = 1
+                }
             }
-            else if (curOpt.id == imgToOpt[1]) { // img2만 활성화
-                binding.ivPostImg1.alpha = 0.5F
-            }
+        } else { // 해당 옵션이 이미지를 갖지 않는다
+            binding.itImgSel1 = 0
+            binding.itImgSel2 = 0
         }
     }
 
