f72e82da43ba2434a9f519194ee0efe74a5bacf6
junwoo <rhffldkt0517@naver.com>
2023-01-13 03:29:04 +0900

    [Feat] Add DetailActiviry

--- app/src/main/java/com/android/hara/presentation/detail/DetailWithActivity.kt
+++ app/src/main/java/com/android/hara/presentation/detail/DetailWithActivity.kt
@@ -7,7 +7,7 @@ import com.android.hara.databinding.ActivityDetailWithBinding
 import com.android.hara.presentation.base.BindingActivity
 import com.android.hara.presentation.custom.DecisionDialog
 import com.android.hara.presentation.custom.model.DialogData
-import com.android.hara.presentation.detail.viewmodel.DetailViewModel
+import com.android.hara.presentation.detail.viewmodel.DetailViewWithModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,10 +15,14 @@ class DetailWithActivity :
     BindingActivity<ActivityDetailWithBinding>(R.layout.activity_detail_with) {
     //TODO [고민글 상세보기] 부분입니다. 추후 네이밍 변경 예정
 
-    private val detailVm by viewModels<DetailViewModel>()
+    private val detailVm by viewModels<DetailViewWithModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val worryId = intent.getIntExtra("worryId", 0)
+        //detailVm.
+
         binding.appbarDetail.setNavigationOnClickListener {
             finish()
         }
