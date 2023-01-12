87b1c7eaf350ec0ce79e3895e0d815678eb46196
junwoo <rhffldkt0517@naver.com>
2023-01-12 19:42:31 +0900

    [Feat] Add SearchActivity

---
+++ app/src/main/java/com/android/hara/presentation/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.android.hara.presentation.search
+
+import android.os.Bundle
+import com.android.hara.R
+import com.android.hara.databinding.ActivitySearchBinding
+import com.android.hara.presentation.base.BindingActivity
+
+class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_search) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
