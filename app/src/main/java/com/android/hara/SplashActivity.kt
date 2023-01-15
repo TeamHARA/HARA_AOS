227d2c395994f569e2a46aa5b93bfb64e4ef5098
IslandofDream <rhffldkt0517@naver.com>
2023-01-15 17:12:33 +0900

    [Refactor] Add Annotations in splashActivity

--- app/src/main/java/com/android/hara/SplashActivity.kt
+++ app/src/main/java/com/android/hara/SplashActivity.kt
@@ -1,13 +1,17 @@
 package com.android.hara
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.android.hara.presentation.home.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
