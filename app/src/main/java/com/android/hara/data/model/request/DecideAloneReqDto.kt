8433c5c0046af7992659a054cb455179c2fa9cec
IslandofDream <rhffldkt0517@naver.com>
2023-01-16 14:32:30 +0900

    [Refactor] Add SerialName annotation

--- app/src/main/java/com/android/hara/data/model/request/DecideAloneReqDto.kt
+++ app/src/main/java/com/android/hara/data/model/request/DecideAloneReqDto.kt
@@ -1,9 +1,12 @@
 package com.android.hara.data.model.request
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DecideAloneReqDto(
+    @SerialName("worryAloneId")
     val worryAloneId: Int,
+    @SerialName("chosenOptionId")
     val chosenOptionId: Int
 )
--- app/src/main/java/com/android/hara/data/model/request/DecideWithReqDto.kt
+++ app/src/main/java/com/android/hara/data/model/request/DecideWithReqDto.kt
@@ -1,9 +1,12 @@
 package com.android.hara.data.model.request
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DecideWithReqDto(
+    @SerialName("worryWithId")
     val worryWithId: Int,
+    @SerialName("chosenOptionId")
     val chosenOptionId: Int
 )
