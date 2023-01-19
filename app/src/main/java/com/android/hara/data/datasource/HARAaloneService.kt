a239b847fb1fd058920be1af5a455125986d2cad
IslandofDream <rhffldkt0517@naver.com>
2023-01-19 19:55:42 +0900

    [Feat] Delete Response Wrapper in Service

--- app/src/main/java/com/android/hara/data/datasource/HARAaloneService.kt
+++ app/src/main/java/com/android/hara/data/datasource/HARAaloneService.kt
@@ -22,39 +22,39 @@ interface HARAaloneService {
     // TODO: Headers를 제거해도 post 요청이 제대로 가는지 확인
     @Headers("Content-Type: application/json")
     @POST("/worry")
-    suspend fun vote(@Body request: VoteReqDto): Response<VoteResDto>
+    suspend fun vote(@Body request: VoteReqDto): VoteResDto
     // suspend fun vote(@Body request:RequestVoteDTO) : Response<ResponseVoteDTO>
     // Response에서 별다른 걸 안해줘도 된다면 == 서버에서 status가 정확하게 온다며 Response 필요없음
 
     @GET("/worry/alone/list/{isSolved}")
-    suspend fun getAloneList(@Path("isSolved") isSolved: Int): Response<WorryListResDto>
+    suspend fun getAloneList(@Path("isSolved") isSolved: Int): WorryListResDto
 
     @GET("/worry/with/list/{isSolved}")
-    suspend fun getWithList(@Path("isSolved") isSolved: Int): Response<WorryListResDto>
+    suspend fun getWithList(@Path("isSolved") isSolved: Int): WorryListResDto
 
     @GET("/random")
-    suspend fun getRandom(): Response<OnesecResDto>
+    suspend fun getRandom(): OnesecResDto
 
     @GET("/random/list")
-    suspend fun getLastWorry(): Response<RandomListResDto>
+    suspend fun getLastWorry(): RandomListResDto
 
     @PATCH("/worry/alone")
-    suspend fun patchAloneDecision(@Body decision: DecideAloneReqDto): Response<DecisionResDto>
+    suspend fun patchAloneDecision(@Body decision: DecideAloneReqDto): DecisionResDto
 
     @PATCH("/worry/with")
-    suspend fun patchWithDecision(@Body decision: DecideWithReqDto): Response<DecisionResDto>
+    suspend fun patchWithDecision(@Body decision: DecideWithReqDto): DecisionResDto
 
     @POST("/worry/with")
-    suspend fun postWorryWith(@Body requset: WorryWithRequestDto): Response<WorryWithResponseDto>
+    suspend fun postWorryWith(@Body requset: WorryWithRequestDto): WorryWithResponseDto
 
     @POST("/worry/alone")
-    suspend fun postWorryAlone(@Body requset: WorryAloneRequestDto): Response<WorryAloneResponseDto>
+    suspend fun postWorryAlone(@Body requset: WorryAloneRequestDto): WorryAloneResponseDto
 
     ///worry/with/:worryId
     @GET("/worry/with/{worryId}")
-    suspend fun getDetailWith(@Path("worryId") worryId: Int): Response<DetailWithResDto>
+    suspend fun getDetailWith(@Path("worryId") worryId: Int): DetailWithResDto
 
     @GET("/worry/alone/{worryId}")
-    suspend fun getDetailAlone(@Path("worryId") worryId: Int): Response<DetailAloneResDto>
+    suspend fun getDetailAlone(@Path("worryId") worryId: Int): DetailAloneResDto
 
 }
